cc010a27038252f0eca716bd95359213e351de9a
AndresBo <anndresfborrero@gmail.com>
2023-07-08 09:29:01 +1000

    build: moved mongoose schema to module in models/blog.js. Also copied API route handlers to module in controllers/blogs.js, this controller module is still not in use by main app

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
-const config = require('./utils/config')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
 
-const Blog = mongoose.model('Blog', blogSchema)
+//!!!
+const Blog = require('./models/blog')
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
