bd6076bc508f70705b704137e2c7b70a8adca2b8
AndresBo <anndresfborrero@gmail.com>
2023-07-09 10:48:55 +1000

    build: created new helper function that gets blog with most likes from a list of blogs. Also created a test for function favoriteBlog

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -10,8 +10,28 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const reducer = (favorite, blog) => {
+    if (favorite.likes > blog.likes) {
+      return favorite
+    } else {
+      return blog
+    }
+  }
+
+  if (blogs.length === 0) {
+    return []
+  } else if (blogs.length === 1) {
+    return blogs[0]
+  }
+
+
+  return blogs.reduce(reducer, blogs[0])
+}
+
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
