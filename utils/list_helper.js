1992dcdaee8cb44beb5f6029a08aa11a71a7d454
AndresBo <anndresfborrero@gmail.com>
2023-07-12 09:54:44 +1000

    build: added helper function 'mostBlogs' with a corresponding group of tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -29,9 +29,39 @@ const favoriteBlog = (blogs) => {
   return blogs.reduce(reducer, blogs[0])
 }
 
+const mostBlogs = (blogs) => {
+  // handle empty lists
+  if (blogs.length === 0) {
+    return {}
+  }
+  // create an array for objects in the form: [{ author: "Robert Martin", blogs: 5 }, ...]
+  // use this array to store authors and their number of blogs
+  let numberOfBlogs = []
+  // iterate through each blog in blogs
+  for (const blog of blogs) {
+    // look for author in numberOfBlogs array
+    const authorIndex = numberOfBlogs.findIndex(entry => entry.author === blog.author)
+    // if the author is in numberOfBlogs, add one to blogs count
+    if (authorIndex !== -1) {
+      numberOfBlogs[authorIndex].blogs ++
+    // if author is not in numberOfBlogs, create a new object for author with blogs count of 1
+    } else {
+      numberOfBlogs.push(
+        { author:blog.author,
+          blogs:1
+        }
+      )
+    }
+  }
+  // default sort order is ascending:
+  numberOfBlogs.sort((a,b) => a.blogs - b.blogs)
+  return numberOfBlogs[numberOfBlogs.length - 1]
+}
+
 
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
