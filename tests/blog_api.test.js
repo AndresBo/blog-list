e947b98e1247277f953a06c99dacb5f2ef27fa7f
AndresBo <anndresfborrero@gmail.com>
2023-07-14 11:08:06 +1000

    build: added test_helper.js module and added beforeEach to blog_api.test.js to initialize db before every test

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
 // wrap express app with supertest so tests can use it to make HTTP requests to the backend
 const api = supertest(app)
+
+// initialize database before tests
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  for (let blog of helper.initialBlogs) {
+    let blogObject = new Blog(blog)
+    await blogObject.save()
+  }
+})
 
 test('blogs are returned in JSON format', async () => {
   await api
---
+++ tests/test_helper.js
@@ -0,0 +1,20 @@
+
+
+const initialBlogs = [
+  {
+    title: 'blog example 1',
+    author: 'a person',
+    url: 'a URL',
+    likes: 1
+  },
+  {
+    title: 'blog example 2',
+    author: 'a person',
+    url: 'a URL',
+    likes: 2
+  }
+]
+
+module.exports = {
+  initialBlogs
+}
