ac66e9e043e6db998e1b5c5b9c4cd97884c2c68f
AndresBo <anndresfborrero@gmail.com>
2023-07-21 09:33:41 +1000

    build: added route for creating users

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 
 mongoose.connect(config.MONGODB_URI)
@@ -16,5 +17,6 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
