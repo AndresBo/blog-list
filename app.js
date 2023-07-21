524922268e0a774e006518c4a961a52f4337d28f
AndresBo <anndresfborrero@gmail.com>
2023-07-21 12:06:38 +1000

    build: added middleware.js to utils folder with errorhandler function, a function that handlers unknown routes and one that prints HTML request information to console.log

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const middleware = require('./utils/middleware')
 
 
 mongoose.connect(config.MONGODB_URI)
@@ -18,5 +19,8 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+
+app.use(middleware.unknownEndPoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
