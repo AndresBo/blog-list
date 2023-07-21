104ba515a45de83eb8b852bbe25d925a5f9806f8
AndresBo <anndresfborrero@gmail.com>
2023-07-21 13:04:58 +1000

    build: added middleware requestLogger to app.js

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 
+app.use(middleware.requestLogger)
+
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 
