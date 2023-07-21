d076e3d3244405e42b6b59950a3501057aa596e2
AndresBo <anndresfborrero@gmail.com>
2023-07-21 10:09:03 +1000

    build: added user testing

---
+++ tests/user_api.test.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const bcrypt = require('bcrypt')
+const app = require('../app')
+const Blog = require('../models/blog')
+const User = require('../models/users')
+const helper = require('./test_helper')
+// wrap express app with supertest so tests can use it to make HTTP requests to the backend
+const api = supertest(app)
+
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('password', 10)
+    const user = new User({ username: 'test', passwordHash, name: 'test' })
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'pablo',
+      name: 'Pablo Martin',
+      password: 'password',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
+})
